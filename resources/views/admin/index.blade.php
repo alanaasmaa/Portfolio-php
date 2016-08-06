797117d6573809048e87ab98b8687c14f022f00f
Alan Aasmaa <alanaasmaa@gmail.com>
2016-08-06 12:51:37 +0300

    Admin panel

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -10,21 +10,25 @@
         <span class="orangeBg">
             <i class="fa fa-cog"></i>
         </span>
+        visits today
      </div>
      <div class="fourth box info-box">
         <span class="redBg">
             <i class="fa fa-cog"></i>
         </span>
+        Unique visitors
      </div>
      <div class="fourth box info-box">
         <span class="oliveBg">
             <i class="fa fa-cog"></i>
         </span>
+        Page views
      </div>
      <div class="fourth box info-box">
         <span class="navyBg">
             <i class="fa fa-cog"></i>
         </span>
+        Today's earnings
      </div>
     </section>
 </section>
--- resources/views/layouts/admin/aside.blade.php
+++ resources/views/layouts/admin/aside.blade.php
@@ -1,8 +1,24 @@
 <aside class="box">
     <header>
         <a href="/profile/{{ Auth::user()->id}}">
-            {{ Auth::user()->name }}
-            <img src="{{ Gravatar::src(Auth::user()->email, 40)  }}">
+            <img src="{{ Gravatar::src(Auth::user()->email, 80)  }}">
         </a>
+        <a href="/profile/{{ Auth::user()->id}}">
+            <h4>{{ Auth::user()->name }}</h4>
+        </a>
+        <p>Owner</p>
+        <ul>
+            <li><a href="index.html"><span>Profile</span><i class="fa fa-user"></i></a></li>
+            <li><a href="index.html"><span>Settings</span><i class="fa fa-cog"></i></a></li>
+            <li><a href="index.html"><span>Log out</span><i class="fa fa-sign-out"></i></a></li>
+        </ul>
     </header>
+    <section>
+    <h6>Navigation</h6>
+    <ul>
+        <li><a href="admin/">Dashboard</a></li>
+        <li><a href="admin/email">Email</a></li>
+        <li><a href="admin/users">Users</a></li>
+    </ul>
+    <section>
 </aside>
